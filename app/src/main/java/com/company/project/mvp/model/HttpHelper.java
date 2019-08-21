a1fe39ce09aeaaa8182ecb15f688fabea5083595
weixuewen <ation_work@163.com>
2019-08-21 16:07:23 +0800

    有密文传输，添加token认证，以后添加SSL

--- app/src/main/java/com/company/project/mvp/model/HttpHelper.java
+++ app/src/main/java/com/company/project/mvp/model/HttpHelper.java
@@ -17,15 +17,17 @@ import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 import okhttp3.Cache;
+import okhttp3.CacheControl;
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
- * Author：leguang on 2016/10/9 0009 15:49
- * Email：langmanleguang@qq.com
  */
 public class HttpHelper {
 
@@ -75,6 +77,40 @@ public class HttpHelper {
             builder.writeTimeout(Constants.DEFAULT_TIMEOUT, TimeUnit.SECONDS);
             //错误重连
             builder.retryOnConnectionFailure(true);
+
+            builder.addInterceptor(new Interceptor() {
+                @Override
+                public Response intercept(Chain chain) throws IOException {
+                    int maxAge = 60 * 60; // 有网络时 设置缓存超时时间1个小时
+                    int maxStale = 60 * 60 * 24 * 28; // 无网络时，设置超时为4周
+                    Request request = chain.request();
+//                    if (IsNetWork.isNetworkReachable(MyApp.getContext())){
+                    request = request.newBuilder()
+                            .addHeader("X-AUTH-TOKEN","token")
+                            .addHeader("Content-Type", "application/x-www-form-urlencoded")
+                            .cacheControl(CacheControl.FORCE_NETWORK)//有网络时只从网络获取
+                            .build();
+                    //添加公共token
+//                    } else {
+//                        request = request.newBuilder()
+//                                .cacheControl(CacheControl.FORCE_CACHE)//无网络时只从缓存中读取
+//                                .build();
+//                    }
+                    Response response = chain.proceed(request);
+//                    if (IsNetWork.isNetworkReachable(MyApp.getContext())) {
+                    response = response.newBuilder()
+                            .removeHeader("Pragma")
+                            .header("Cache-Control", "public, max-age=" + maxAge)
+                            .build();
+//                    } else {
+//                        response = response.newBuilder()
+//                                .removeHeader("Pragma")
+//                                .header("Cache-Control", "public, only-if-cached, max-stale=" + maxStale)
+//                                .build();
+//                    }
+                    return response;
+                }
+            });
 
             //DEBUG模式下配Log拦截器
             if (BuildConfig.DEBUG) {
